ac4a12b718e91d736668453d3b2abafe43a83fa7
Marika <marika.sadowska00@gmail.com>
2026-01-20 14:31:31 +0000

    New method isValidTotalPurchase and moved customer class to its own file

--- src/main/java/com/mycompany/readfromfile/ReadFromFile.java
+++ src/main/java/com/mycompany/readfromfile/ReadFromFile.java
@@ -49,20 +49,24 @@ public class ReadFromFile {
         
         return true;
     }
-}
-
-public class Customer {
-    String firstName;
-    String lastName;
-    float totalPurchase;
-    int customerClass;
-    int lastPurchaseYear;
     
-    public Customer(String name, String surname, float purchase, int customClass, int year){
-        firstName = name;
-        lastName = surname;
-        totalPurchase = purchase;
-        customerClass = customClass;
-        lastPurchaseYear = year;
+     // Validating if the last name in the customers.txt only contains letters
+    public static boolean isValidTotalPurchase(String purchase){
+        char[] chars = purchase.toCharArray();
+        int decimalCounter = 0;
+        
+        for (char c : chars){
+            if(!Character.isDigit(c) && !(c == '.')){
+                return false;
+            }
+            else if (c == '.'){
+                decimalCounter += 1;
+                    if (decimalCounter > 1){
+                        return false;
+                    }
+                }
+        }
+        
+        return true;
     }
 }
